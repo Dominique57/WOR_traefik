96ca25abb7697e2b5f6da84dcbfa500814cd082d
Dominique MICHEL <dominique.michel@epita.fr>
2025-01-17 08:41:37 +0100

    [+] add sample config

--- src/github.com/dominique57/wor_traefik/wor_traefik.go
+++ src/github.com/dominique57/wor_traefik/wor_traefik.go
@@ -2,14 +2,18 @@ package wor_traefik
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 )
 
 type Config struct {
+    Foo string
 }
 
 func CreateConfig() *Config {
-	return &Config{}
+	return &Config{
+        Foo: "",
+	}
 }
 
 type WorDemo struct {
@@ -18,6 +22,10 @@ type WorDemo struct {
 }
 
 func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
+    if len(config.Foo) == 0 {
+        return nil, fmt.Errorf("Config Foo cannot be empty !");
+    }
+
 	return &WorDemo{
 		next: next,
 		name: name,
